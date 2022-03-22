132539124288d7dd08c2536d9ca375d109ad464f
Mehedi Hasan Shuvo <duetianmehedishuvo@gmail.com>
2022-03-23 02:19:58 +0600

    t

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         catchError: (_, __) => MyUser(),
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
-          title: 'Flutter Demo',
+          title: 'DUET Clinic',
           theme: ThemeData(
             primarySwatch: Colors.teal,
             visualDensity: VisualDensity.adaptivePlatformDensity,
